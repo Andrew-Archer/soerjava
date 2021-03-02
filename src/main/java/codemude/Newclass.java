1ff78120b12cd7c531e2b73c02cfa493ae00906f
Razumnov Andrey Alexandrovich <razandale@gmail.com>
2021-03-02 23:13:05 +0400

    Add comments.
    Read the comments and try to understand what each code word means.

--- src/main/java/codemude/Newclass.java
+++ src/main/java/codemude/Newclass.java
@@ -1,7 +1,17 @@
+//define package in which our class lives. 
 package codemude;
 
-public class Newclass {
+//Declare public class named NewClass
+public class NewClass {
+    //Declare public static function
+    //returning void named main
+    //with parameter of strings array type.
+    //This method will be called by JVM
+    //to start program. 
     public static void main(String[] initial) {
+        //Call static method of static field 
+        //of System class with string argument
+        //to print message
         System.out.println("Hello, dude");
     }
 }
